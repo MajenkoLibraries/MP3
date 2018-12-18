4470537ea9829f94b6c1585373b8260642fe410c
Matt Jenkins <matt@majenko.co.uk>
2018-12-18 21:31:18 +0000

    Ported to I2S AudioSource class

--- MP3.cpp
+++ MP3.cpp
@@ -1,16 +1,25 @@
 #include <MP3.h>
 
 int MP3::readBytesFromStream() {
-    memmove(_readBuf, _readPtr, _bytesLeft);
-    uint32_t nRead = _source->readBytes(_readBuf + _bytesLeft, READBUF_SIZE - _bytesLeft);
-    memset(_readBuf + _bytesLeft + nRead, 0, READBUF_SIZE - _bytesLeft - nRead);
-    _bytesLeft += nRead;
+    // Step one - pull down the remaining part of the buffer to the start of memory
+    if (_bytesLeft > 0) {
+        memmove(_readBuf, _readPtr, _bytesLeft);
+    }
+    uint32_t nr;
+    int rv;
+
+    // Step two - fill the latter part of the buffer (now empty) with more data.
+    if ((rv = _file->fsread(_readBuf + _bytesLeft, READBUF_SIZE - _bytesLeft, &nr, (READBUF_SIZE - _bytesLeft) / 512 + 1)) != FR_OK) {
+        return -1;
+    }
+//    memset(_readBuf + _bytesLeft + nr, 0, READBUF_SIZE - _bytesLeft - nr);
+    _bytesLeft += nr;
     _readPtr = _readBuf;
-    return nRead;
+    return nr;
 }
 
-MP3::MP3(Stream &str) {
-    _source = &str;
+MP3::MP3(DFILE *file) {
+    _file = file; 
     _mp3DecInfo.FrameHeaderPS =     (void *)&_frameHeader;
     _mp3DecInfo.SideInfoPS =        (void *)&_sideInfo;
     _mp3DecInfo.ScaleFactorInfoPS = (void *)&_scaleFactorInfo;
@@ -38,26 +47,41 @@ int MP3::decodeFrameInfo(uint8_t *ptr) {
 
 int MP3::decodeStream(uint8_t **ptr, int *len, int16_t *outBuf) {
     return MP3Decode(&_mp3DecInfo, ptr, len, outBuf, 0);
-
 }
 
-int MP3::begin() {
+void MP3::initStream() {
+    _file->fslseek(0);
     int offset = -1;
-    while (offset < 0) {
-        _bytesLeft = _source->readBytes(_readBuf, READBUF_SIZE);
-        if (_bytesLeft == 0) {
-            return -1;
-        }
-        _readPtr = _readBuf;
-        offset = MP3FindSyncWord(_readPtr, _bytesLeft); 
-    }
-    _readPtr += offset;
-    _bytesLeft -= offset;
-    int err = decodeFrameInfo(_readPtr);
-    Serial.printf("Channels: %d\r\n", _frameInfo.nChans);
-    Serial.printf("Sample Rate: %d\r\n", _frameInfo.samprate);
+    _bytesLeft = 0;
+    bool found = false; 
+    while (!found) {
+        while (offset < 0) {
+            uint32_t nr;
 
-    readBytesFromStream();
+            nr = readBytesFromStream();
+            if (nr < 0) {
+                return;
+            }
+
+            offset = MP3FindSyncWord(_readPtr, nr); 
+
+            if (offset < 0) { // Discard the buffer.
+                _bytesLeft = 0;
+            } else {
+                _readPtr = _readBuf + offset;
+                _bytesLeft -= offset;
+            }
+        }
+        offset = -1;
+
+        // We have a sync word. Let's top up the buffer.
+        readBytesFromStream();
+        // This should have shifted the sync word to the start of the buffer.
+        int err = decodeFrameInfo(_readPtr);
+        if (err >= 0) {
+            found = true;
+        }
+    }
 }
 
 int MP3::decodeNewChunk() {
@@ -68,17 +92,14 @@ int MP3::decodeNewChunk() {
             case 0:
             case -2: // Read more.
                 if (readBytesFromStream() <= 0) {
-Serial.println("EOF");
                     return -2;
                 }
                 break;
             default:
-Serial.print("Error: ");
-Serial.println(err);
                 return err;
         }
     } while (err != 0);
-    _numSamples = 1152;
+    _numSamples = _mp3DecInfo.nGrans * _mp3DecInfo.nGranSamps * _mp3DecInfo.nChans;
     _outPos = 0;
     return err;
 }
@@ -92,4 +113,23 @@ int MP3::read() {
         }
     }
     return _outBuf[_outPos++];
+}
+
+uint32_t MP3::getNextSampleBlock(int16_t *samples, uint32_t len) {
+    for (int i = 0; i < len; i++) {
+        int v = read();
+        if (v == -70000) {
+            return i;
+        }
+        samples[i] = v;
+    }
+    return len;
+}
+
+uint32_t MP3::getFrameLength() {
+    return _frameInfo.outputSamps;
+}
+
+uint32_t MP3::getChannels() {
+    return _frameInfo.nChans;
 }
--- MP3.h
+++ MP3.h
@@ -2,14 +2,19 @@
 #define _MP3_H
 
 #include <Arduino.h>
+#include <I2S.h>
+#include <DFATFS.h>
 
 #include "utility/mp3dec.h"
 #include "utility/coder.h"
 
 #define READBUF_SIZE 1940
 
-class MP3 : public Stream {
+class MP3 : public AudioSource {
     private:
+
+        DFILE *_file;
+
         FrameHeader _frameHeader;
         SideInfo _sideInfo;
         ScaleFactorInfo _scaleFactorInfo;
@@ -20,6 +25,8 @@ class MP3 : public Stream {
 
         MP3DecInfo _mp3DecInfo;
 
+        int read();
+
         int16_t _outBuf[2 * 1152];
         uint32_t _outPos;
         uint32_t _numSamples;
@@ -27,26 +34,23 @@ class MP3 : public Stream {
         uint8_t *_readPtr;
         int _bytesLeft;
 
-        Stream *_source;
-
         int readBytesFromStream();
+        int decodeFrameInfo(uint8_t *ptr);
+        int decodeStream(uint8_t **ptr, int *len, int16_t *outBuf);
+        int decodeNewChunk();
 
     public:
         MP3FrameInfo _frameInfo;
 
-        MP3(Stream &str);
+        MP3(DFILE *file);
+        MP3(DFILE &file) : MP3(&file) {}
 
-        int decodeFrameInfo(uint8_t *ptr);
-        int decodeStream(uint8_t **ptr, int *len, int16_t *outBuf);
-        int begin();
-        int decodeNewChunk();
-        size_t write(uint8_t) { return 0; }
-        int available() { return _numSamples - _outPos; }
-        int read();
-        int peek() {
-            return 0; // Later...!
-        }
-        void flush() { };
+        void initStream();
+
+        uint32_t getNextSampleBlock(int16_t *samples, uint32_t len);
+        uint32_t getFrameLength();
+        uint32_t getChannels();
+        uint32_t seekToFrame(uint32_t f) { return 0; }
 };
 
 
